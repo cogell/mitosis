f82acf615a44c20d854a0cf9ae557262ffe2f24f
B. Cedric Cogell <cedric.cogell@gmail.com>
2013-12-08 15:10:22 -0500

    adding new comment form with some base styling

--- app/public/require_config.js
+++ app/public/require_config.js
@@ -1,7 +1,8 @@
 require.config({
   paths:{
     jquery: 'vendor/jquery/jquery',
-    bootstrap: 'vendor/bootstrap/dist/js/bootstrap.js'
+    bootstrap: 'vendor/bootstrap/dist/js/bootstrap.js',
+    bootstrap: 'vendor/masonry/masonry.js'
   },
   shim: {
     'bootstrap': ['jquery']
