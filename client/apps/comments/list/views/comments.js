09e7f79f0d739b58e7ed261e3d5ac2e7e2418241
B. Cedric Cogell <cedric.cogell@gmail.com>
2013-12-11 12:52:05 -0500

    wires up packery to work on comments collection view and when new items are added to that view

--- client/apps/comments/list/views/comments.js
+++ client/apps/comments/list/views/comments.js
@@ -1,6 +1,8 @@
 define(function(require){
 
   var App = require('app');
+  var packeryPkg = require('packeryPkg');
+  var Packery = require('packery');
   require('Handlebars');
   require('apps/comments/list/views/comment');
 
@@ -8,7 +10,30 @@ define(function(require){
 
     List.Comments = Marionette.CollectionView.extend({
       itemView: List.Comment,
-      itemViewContainer: '.packery-container'
+      itemViewContainer: '.packery-container',
+      className: 'comments-container',
+      onShow: function(){
+        this.pckryReload();
+      },
+      onAfterItemAdded: function(itemView){
+        this.pckryReload();
+      },
+      pckryReload: function(){
+        console.log('test');
+        if (this.pckry){
+          this.pckry.packery('reload');
+        } else {
+          this.initPckry();
+        }
+      },
+      initPckry: function(){
+        // console.log('this.$el', this.$el);
+        this.pcky = new Packery(this.$el[0], {
+          // options
+          itemSelector: '.item',
+          gutter: 0
+        });
+      }
     })
 
   });
